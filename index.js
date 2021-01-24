d6abfb951f8263e3b2d036eeb788116fb69cda2e
Leah Perri <leahperri2017@u.northwestern.edu>
2021-01-24 13:42:07 -0800

    Connect MongoDB via Mongoose (#3)
    * add user model class and load the user collection into mongoose
    * require in mongoose in index.js plus connect to mongo

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./server/config/keys');
+const mongoURI = require('./server/config/keys').mongoURI;
+// order of these next two lines MATTERS
+// otherwise: "MissingSchemaError: Schema hasn't been registered for model 'users'."
+require('./server/models/User');
 require('./server/services/passport');
+
+mongoose.connect(mongoURI);
 
 const app = express();
 
